81bc651c60c14bf910c6af52fdeb45e8e832ce64
graffe <graffe@graffe.co>
2024-01-23 12:46:32 -0800

    fixing up setttings

--- src/App.tsx
+++ src/App.tsx
@@ -12,25 +12,28 @@ function App() {
             .then((res) => setApiResponse(res))
         .catch((err) => setApiResponse(JSON.stringify(err)));
     }, []);
+
+    const [apiResponseWithJs,setApiResponseWithJs] = useState<string|undefined>();
+
+    useEffect(() => {
+        fetch("/api/function_js")
+            .then((res) => res.text())
+            .then((res) => setApiResponseWithJs(res))
+            .catch((err) => setApiResponseWithJs(JSON.stringify(err)));
+    }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
-          <div>API Response:</div>
+          <div><strong>API Response w/ no file extension import:</strong></div>
           <div>{!apiResponse && 'Loading...'}</div>
         <div>{apiResponse &&  apiResponse}</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="card">
+            <div><strong>API Response w/ .js import:</strong></div>
+            <div>{!apiResponseWithJs && 'Loading...'}</div>
+            <div>{apiResponseWithJs &&  apiResponseWithJs}</div>
+        </div>
     </>
   )
 }
