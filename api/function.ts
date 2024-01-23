da642377e1dc8b33c56104c55b88eb7dbd51ceca
graffe <graffe@graffe.co>
2024-01-23 12:00:56 -0800

    function

---
+++ api/function.ts
@@ -0,0 +1,6 @@
+
+// @ts-expect-error not using req
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    // console.log(req);
+    res.status(200).send({hello:"dude"});
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [apiResponse,setApiResponse] = useState<string|undefined>();
 
+    useEffect(() => {
+        fetch("/api/function")
+            .then((res) => res.text())
+            .then((res) => setApiResponse(res))
+        .catch((err) => setApiResponse(JSON.stringify(err)));
+    }, []);
   return (
     <>
       <div>
@@ -18,12 +24,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+          <div>API Response:</div>
+          <div>{!apiResponse && 'Loading...'}</div>
+        <div>{apiResponse &&  apiResponse}</div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
