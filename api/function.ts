042da29f0cc33da6919e221ea054bf3b6354a62b
Nick Vigier <nvigier46@gmail.com>
2024-01-26 08:59:44 +0000

    Use esbuild to bundle api functions

--- api/function.ts
+++
@@ -1,9 +0,0 @@
-
-
-import {reply} from "./helper";
-
-// @ts-expect-error not using req
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-    // console.log(req);
-    res.status(200).send({hello:reply});
-}
--- api/helper.ts
+++
@@ -1,3 +0,0 @@
-
-
-export const reply = "dude its ok";
